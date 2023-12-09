740e40384ae81861a61edbde8e1b012a1df1f3f7
Alexey Parshin <jax77@mail.ru>
2023-12-09 16:10:35 +0300

    Выполнил ДЗ по модулю номер 8

---
+++ module08/src/main/java/ru/sberbank/edu/Main.java
@@ -0,0 +1,37 @@
+package ru.sberbank.edu;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+import java.io.File;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static final String CONTEXT = "/";
+    public static final int PORT = 8080;
+
+    public static void main(String[] args) throws LifecycleException {
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setBaseDir("temp");
+        tomcat.setPort(PORT);
+
+        StandardContext context = (StandardContext) tomcat.addWebapp(CONTEXT, new File("src/main/webapp/").getAbsolutePath());
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(context);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
+        context.setResources(resources);
+
+        tomcat.getConnector();
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
---
+++ module08/src/main/webapp/author.jsp
@@ -0,0 +1,19 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <style>
+        <%@include file="/WEB-INF/css/style.css"%>
+    </style>
+    <title>author</title>
+</head>
+<body>
+<h1>АВТОР РАБОТЫ</h1>
+<div class="author">
+    <span>
+        Работу выполнил Паршин Алексей Юрьевич
+    </span>
+</div>
+<hr>
+<a href="/index.jsp">Назад</a>
+</body>
+</html>
